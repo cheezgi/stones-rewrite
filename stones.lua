d3dc5128b40ee6ea0e6cf88d149bd2abf586ffcb
cheezgi <zackh@firemail.cc>
2016-11-10 23:40:58 +0000

    add argparse

--- stones.lua
+++ stones.lua
@@ -2,11 +2,19 @@
 -- external libraries
 local microlight = require("libs.microlight")
 local argparse = require("libs.argparse")
+        ("stones", "Esoteric programming language")
 
 -- internal libraries
 local syntax = require("syntax")
 local field = require("field")
 
-local stoneColors = syntax.colors
-local directions = syntax.dirs
+local stoneColors = syntax.stoneColors
+local directions = syntax.directions
+
+argparse:argument("file", "File to be interpreted")
+parser:flag("-f --field", "Print field")
+parser:flag("-r --frames", "Print frames")
+parser:flag("-d --debug", "Print debugging")
+parser:flag("-s --stack", "Print stack")
+parser:flag("-v --version", "Print version")
 
