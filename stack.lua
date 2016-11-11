e51c976549334b8acf2129358110b7f9b944d884
cheezgi <zackh@firemail.cc>
2016-11-11 01:29:15 +0000

    add stack

---
+++ stack.lua
@@ -0,0 +1,31 @@
+
+Stack = {}
+Stack.__index = Stack
+
+setmetatable(Stack, {
+    __call = function(cls, ...)
+        return cls.new(...)
+    end
+})
+
+function Stack.new()
+    local self = setmetatable({}, Stack)
+    self.table = {}
+    return self
+end
+
+function Stack.get(self)
+    return self.table
+end
+
+function Stack.push(self, val)
+    table.insert(self.table, val)
+end
+
+function Stack.pop(self)
+    return table.remove(self.table)
+end
+
+function Stack.last(self)
+    return self.table[#self.table]
+end
